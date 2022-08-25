d62825dcc735aaf1c7bca2be2ae444c69468af6c
Jacks2019 <964800156@qq.com>
2022-08-25 15:36:07 +0800

    增加Frequency的枚举类型

---
+++ src/main/java/com/citi/backend/enums/FrequencyEnum.java
@@ -0,0 +1,5 @@
+package com.citi.backend.enums;
+
+public enum FrequencyEnum {
+    _1D, _1W, _2W, _1M, _3M, _6M, _1Y, _YTD
+}
--- src/main/java/com/citi/backend/service/impl/TradeServiceImpl.java
+++ src/main/java/com/citi/backend/service/impl/TradeServiceImpl.java
@@ -2,11 +2,17 @@ package com.citi.backend.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import com.citi.backend.dao.TradeMapper;
 import com.citi.backend.entity.Trade;
 import com.citi.backend.service.TradeService;
 
 public class TradeServiceImpl implements TradeService {
 
+    @Resource
+    TradeMapper tradeMapper;
+    
     @Override
     public List<Trade> getTrade() {
         return null;
