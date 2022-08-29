84ed7f169e08118414ea251fe7f369505722d17a
Jacks2019 <964800156@qq.com>
2022-08-29 15:50:56 +0800

    Controller改为RestController

--- src/main/java/com/citi/backend/controller/TradeController.java
+++ src/main/java/com/citi/backend/controller/TradeController.java
@@ -4,22 +4,20 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.citi.backend.entity.Trade;
 import com.citi.backend.enums.FrequencyEnum;
 import com.citi.backend.service.TradeService;
 
-@Controller
+@RestController
 public class TradeController {
     @Resource
     TradeService tradeService;
 
     @GetMapping("/getTableData")
-    @ResponseBody
     @CrossOrigin
     public List<Trade> getTrade(String fre, int pageSize, int currentPage){
         List<Trade> trades = tradeService.getTrade(FrequencyEnum.valueOf("_" + fre), pageSize, currentPage);
