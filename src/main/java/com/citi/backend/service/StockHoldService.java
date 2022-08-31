2136a1474bfc9552baa42a0effaf969f4de195d1
Jacks2019 <964800156@qq.com>
2022-08-31 15:12:14 +0800

    删除SpringBoot的文件

--- src/main/java/com/citi/backend/service/StockHoldService.java
+++
@@ -1,9 +0,0 @@
-package com.citi.backend.service;
-
-import java.util.List;
-
-import com.citi.backend.entity.StockHold;
-
-public interface StockHoldService {
-    public List<StockHold> getStockHold(int clientId);
-}
